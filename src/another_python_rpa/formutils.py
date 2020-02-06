af3e97154a319d204adb547f7fd1a945176e4f4c
Arthur Aleksandro Alves Silva <aretw0@hotmail.com>
2020-02-06 01:49:11 -0300

    Processamento de formulário já estável

--- src/another_python_rpa/formutils.py
+++ src/another_python_rpa/formutils.py
@@ -18,7 +18,10 @@ Note: This skeleton file can be safely removed if not needed!
 import argparse
 import sys
 import logging
-import xlrd
+import asyncio
+import urllib.request
+from bs4 import BeautifulSoup
+import requests, warnings
 
 from another_python_rpa import __version__
 
@@ -28,19 +31,42 @@ __license__ = "mit"
 
 _logger = logging.getLogger(__name__)
 
+async def post_answers(form_url, questions, answers, verbose=False):
+  if not len(questions) == len(answers):
+    raise Exception("A quantidade de questões e respostas não é a mesma")
+  
+  submit_url = form_url.replace('/viewform', '/formResponse')
+  form_data = {'draftResponse':[],
+              'pageHistory':0}
+  for q, a in zip(questions, answers):
+    form_data[q] = a
+  
+  if verbose:
+    print(form_data)
+  user_agent = {'Referer':form_url,
+                'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
+  res = requests.post(submit_url, data=form_data, headers=user_agent)
+  return res.status_code
 
-def util(url, op):
-    """Leitor e Operador de formulários google
-
-    Args:
-      url (string): URL do formulário
-      op (string): Operação a ser realizada
-
-    Returns:
-      str: Informações sobre a operação escolhida
-    """
+async def get_questions(in_url):
+  try:
+    res = urllib.request.urlopen(in_url)
+    soupHtml = BeautifulSoup(res.read(), 'html.parser')
+    get_labels = lambda f: [v for k, v in f.attrs.items() if 'label' in k]
+    get_label = lambda f, n: get_labels(f)[0] if len(get_labels(f))>0 else n
+    all_questions = soupHtml.form.findChildren(attrs={'name': lambda x: x and x.startswith('entry.')})
+    all_div_labels = soupHtml.form.findChildren(attrs={'class': lambda x: x and x.startswith('freebirdFormviewerViewItemsItemItemTitle ')})
+    all_labels = [str(k).strip() for k, v in all_div_labels]
+    if len(all_labels) == len(all_questions):
+      return {get_label(q, l): q['name'] for q, l in zip(all_questions, all_labels)}   
+    else:
+      raise Exception("A quantidade de questões e títulos não é a mesma")
+  except urllib.error.URLError as error:
+    print(error)
+    print("Falha na obtenção do formulário")
     
-
+  
+  
 
 
 def parse_args(args):
@@ -65,11 +91,13 @@ def parse_args(args):
     groupExcl.add_argument(
       "--get",
       dest="op",
-      action="store_true")
+      action="store_true",
+      help="Retorna a relação de títulos e inputs das questões do formulário")
     groupExcl.add_argument(
       "--post",
       dest="op",
-      action="store_false")
+      action="store_false",
+      help="Submete o formulário com as respostas")
 
     groupPost = parser.add_argument_group("POST")
    
@@ -80,22 +108,22 @@ def parse_args(args):
       metavar="URL")
     
     groupPost.add_argument(
-      "--questans",
-      "-qa",
-      dest="questans",
-      help="Questões do formulário com respostas juntas por delimitador",
+      "--questions",
+      "-q",
+      dest="questions",
+      help="Questões do formulário no formato 'entry.99999[*]'",
       nargs="+",
       required='--post' in sys.argv,
-      metavar="QUESTS & ANS")
-    
-    groupPost.add_argument(
-      "--delim",
-      "-dl",
-      dest="delim",
-      help="Delimitador, o padrão é '#delim#'",
-      type=str)
+      metavar="QUESTIONS")
 
-    parser.set_defaults(delim="#delim#")
+    groupPost.add_argument(
+      "--answers",
+      "-a",
+      dest="answers",
+      help="Respostas do formulário",
+      nargs="+",
+      required='--post' in sys.argv,
+      metavar="ANSWERS")
 
     parser.add_argument(
         "-v",
@@ -135,14 +163,21 @@ def main(args):
     setup_logging(args.loglevel)
     _logger.debug("Iniciando processamento...")
     print("O formulário {} passará pela operação {}".format(args.url, "GET" if args.op else "POST"))
+    if args.op:
+      print("Questões:")
+      print(asyncio.run(get_questions(args.url)))
+    else:
+      print("A submeter...")
+      print("Resultado: {}".format(asyncio.run(post_answers(args.url, args.questions, args.answers))))
+     
     _logger.info("Script ends here")
 
 
 def run():
-    """Entry point for console_scripts
-    """
-    main(sys.argv[1:])
+  """Entry point for console_scripts
+  """
+  main(sys.argv[1:])
 
 
 if __name__ == "__main__":
-    run()
+  run()
